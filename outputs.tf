ee2d025a2ecd76a7c9d2e0ebaa22f24ef1b9cbb2
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:32:27 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "system_center_virtual_machine_manager_availability_sets_id" {
+  description = "Map of id values across all system_center_virtual_machine_manager_availability_sets, keyed the same as var.system_center_virtual_machine_manager_availability_sets"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_availability_set.system_center_virtual_machine_manager_availability_sets : k => v.id }
+}
 output "system_center_virtual_machine_manager_availability_sets_custom_location_id" {
   description = "Map of custom_location_id values across all system_center_virtual_machine_manager_availability_sets, keyed the same as var.system_center_virtual_machine_manager_availability_sets"
   value       = { for k, v in azurerm_system_center_virtual_machine_manager_availability_set.system_center_virtual_machine_manager_availability_sets : k => v.custom_location_id }
