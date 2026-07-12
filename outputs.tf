f0db7a7e90ccdc20732af9b10d12ffb47dba4cb5
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:12 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "system_center_virtual_machine_manager_availability_sets" {
-  description = "All system_center_virtual_machine_manager_availability_set resources"
-  value       = azurerm_system_center_virtual_machine_manager_availability_set.system_center_virtual_machine_manager_availability_sets
-}
 output "system_center_virtual_machine_manager_availability_sets_custom_location_id" {
-  description = "List of custom_location_id values across all system_center_virtual_machine_manager_availability_sets"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_availability_set.system_center_virtual_machine_manager_availability_sets : v.custom_location_id]
+  description = "Map of custom_location_id values across all system_center_virtual_machine_manager_availability_sets, keyed the same as var.system_center_virtual_machine_manager_availability_sets"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_availability_set.system_center_virtual_machine_manager_availability_sets : k => v.custom_location_id }
 }
 output "system_center_virtual_machine_manager_availability_sets_location" {
-  description = "List of location values across all system_center_virtual_machine_manager_availability_sets"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_availability_set.system_center_virtual_machine_manager_availability_sets : v.location]
+  description = "Map of location values across all system_center_virtual_machine_manager_availability_sets, keyed the same as var.system_center_virtual_machine_manager_availability_sets"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_availability_set.system_center_virtual_machine_manager_availability_sets : k => v.location }
 }
 output "system_center_virtual_machine_manager_availability_sets_name" {
-  description = "List of name values across all system_center_virtual_machine_manager_availability_sets"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_availability_set.system_center_virtual_machine_manager_availability_sets : v.name]
+  description = "Map of name values across all system_center_virtual_machine_manager_availability_sets, keyed the same as var.system_center_virtual_machine_manager_availability_sets"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_availability_set.system_center_virtual_machine_manager_availability_sets : k => v.name }
 }
 output "system_center_virtual_machine_manager_availability_sets_resource_group_name" {
-  description = "List of resource_group_name values across all system_center_virtual_machine_manager_availability_sets"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_availability_set.system_center_virtual_machine_manager_availability_sets : v.resource_group_name]
+  description = "Map of resource_group_name values across all system_center_virtual_machine_manager_availability_sets, keyed the same as var.system_center_virtual_machine_manager_availability_sets"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_availability_set.system_center_virtual_machine_manager_availability_sets : k => v.resource_group_name }
 }
 output "system_center_virtual_machine_manager_availability_sets_system_center_virtual_machine_manager_server_id" {
-  description = "List of system_center_virtual_machine_manager_server_id values across all system_center_virtual_machine_manager_availability_sets"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_availability_set.system_center_virtual_machine_manager_availability_sets : v.system_center_virtual_machine_manager_server_id]
+  description = "Map of system_center_virtual_machine_manager_server_id values across all system_center_virtual_machine_manager_availability_sets, keyed the same as var.system_center_virtual_machine_manager_availability_sets"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_availability_set.system_center_virtual_machine_manager_availability_sets : k => v.system_center_virtual_machine_manager_server_id }
 }
 output "system_center_virtual_machine_manager_availability_sets_tags" {
-  description = "List of tags values across all system_center_virtual_machine_manager_availability_sets"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_availability_set.system_center_virtual_machine_manager_availability_sets : v.tags]
+  description = "Map of tags values across all system_center_virtual_machine_manager_availability_sets, keyed the same as var.system_center_virtual_machine_manager_availability_sets"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_availability_set.system_center_virtual_machine_manager_availability_sets : k => v.tags }
 }
 
